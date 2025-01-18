569be25235683cc0ff451c796a6fd4967ffd5f0e
xingzhu <2072648310@qq.com>
2025-01-18 15:34:45 +0800

    Site updated: 2025-01-18 15:34:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/20/hello-world/"];function toRandomPost(){
+var posts=["2024/09/20/hello-world/","2025/01/18/google/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
