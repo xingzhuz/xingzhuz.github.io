ada46192a5d2f54100b62a95b5803fe3a4c90a9e
xingzhu <2072648310@qq.com>
2025-01-18 17:27:38 +0800

    Site updated: 2025-01-18 17:27:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["goole-account/google/","undefined/hello-world/"];function toRandomPost(){
+var posts=["配置/google/","uncategorized/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
