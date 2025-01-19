3f0a897409248bc89b082e74d2530cc63ab0a1ef
xingzhu <2072648310@qq.com>
2025-01-19 15:59:31 +0800

    Site updated: 2025-01-19 15:59:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["uncategorized/hello-world/","配置/google/","配置/hexo-github/"];function toRandomPost(){
+var posts=["配置/google/","uncategorized/hello-world/","配置/hexo-github/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
