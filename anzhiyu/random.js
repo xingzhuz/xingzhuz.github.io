4e8f4811c0ecc779e6e92061a36f25b7efc5e08c
xingzhu <2072648310@qq.com>
2025-01-20 20:40:41 +0800

    Site updated: 2025-01-20 20:40:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["配置/google/","配置/hexo-github/","Linux/git/"];function toRandomPost(){
+var posts=["配置/google/","配置/hexo-github/","Linux/git/","Linux/message-queue/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
