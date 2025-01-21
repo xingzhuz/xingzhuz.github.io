5ed73666fb23a58c278c557a4387f3d9e669fbcc
xingzhu <2072648310@qq.com>
2025-01-21 13:42:31 +0800

    Site updated: 2025-01-21 13:42:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["配置/google/","配置/hexo-github/","Linux/git/","Linux/message-queue/","cpp/lambda/","cpp/smart-pointer/","cpp/thread-threadpool/","cpp/wrapper-binder/","Linux/thread-linux/","cpp/threadpool-cpp/","Linux/bashshell/","Python/python-base/"];function toRandomPost(){
+var posts=["配置/google/","配置/hexo-github/","Linux/git/","Linux/message-queue/","cpp/lambda/","cpp/smart-pointer/","cpp/thread-threadpool/","cpp/wrapper-binder/","Linux/thread-linux/","cpp/threadpool-cpp/","Linux/bashshell/","Python/python-base/","QT/qt-base/","QT/qt-signal-slot/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
