d51e28f087e3cf7e760169992adee444550f2df5
xingzhu <2072648310@qq.com>
2025-01-21 20:03:12 +0800

    Site updated: 2025-01-21 20:02:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Linux/bashshell/","Linux/git/","配置/google/","配置/hexo-github/","cpp/lambda/","Linux/message-queue/","Python/python-base/","QT/qt-signal-slot/","QT/qt-base/","QT/qt-widget/","cpp/smart-pointer/","Linux/thread-linux/","cpp/thread-threadpool/","cpp/threadpool-cpp/","cpp/wrapper-binder/","cpp/move-forward/","QT/qt-draw/"];function toRandomPost(){
+var posts=["Linux/bashshell/","Linux/git/","配置/google/","配置/hexo-github/","cpp/lambda/","Linux/message-queue/","Python/python-base/","QT/qt-signal-slot/","QT/qt-base/","QT/qt-widget/","cpp/smart-pointer/","Linux/thread-linux/","cpp/thread-threadpool/","cpp/threadpool-cpp/","cpp/wrapper-binder/","cpp/move-forward/","QT/qt-draw/","cpp/cpp-keywords/","Linux/http/","cpp/cpp-operator/","Linux/http-server/","cpp/jsoncpp/","杂集/math-model/","MySQL/mysql-connect-pool/","配置/mysqlpool-config/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
